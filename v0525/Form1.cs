791ba84c2b2ac26e4cd5201ba5666f26f67a8b59
田渕 <tabuti.dat@gmail.com>
2023-05-25 16:10:24 +0900

    演習7

--- v0525/Form1.cs
+++ v0525/Form1.cs
@@ -2,9 +2,54 @@ namespace v0525
 {
     public partial class Form1 : Form
     {
+        int vx = 20;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+            label1.Text = "Hello";
+            MessageBox.Show($"‚±‚ñ‚É‚¿‚Í{textBox1.Text}‚³‚ñ");
+            timer1.Enabled = true;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left++;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int vx = 5;
+            MessageBox.Show($"{vx}");
+
+            vx = 10;
+            MessageBox.Show($"{vx}");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"{vx}");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            vx += 10;
+            MessageBox.Show($"{vx}");
         }
     }
 }
